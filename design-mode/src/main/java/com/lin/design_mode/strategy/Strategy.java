c11441821aab4bddae538d6da702123e5ee1a55d
lwb <lwb207628@163.com>
2016-01-12 23:21:32 +0800

    build-log4j

--- design-mode/src/main/java/com/lin/design_mode/strategy/Strategy.java
+++
@@ -1,9 +0,0 @@
-package com.lin.design_mode.strategy;
-
-/**
- * Created by Administrator on 2016/1/8.
- */
-public interface Strategy {
-    //算法方法
-    public void algorithmInterface();
-}
---
+++ spring-test/src/test/java/com/lin/spring_test/BeanFactoryTest.java
@@ -0,0 +1,14 @@
+package com.lin.spring_test;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+
+/**
+ * Created by lwb on 16/1/12.
+ */
+public class BeanFactoryTest {
+    @Test
+    public void testBeanFactory(){
+
+    }
+}
